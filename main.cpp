a4a853605ade97a062ed8a50069f2132518fbb2f
Rafał Curzydło <rafall.curzydlo@gmail.com>
2025-11-27 14:25:12 +0100

    template

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+    main.cpp
+    Szablon aplikacji w C++ (C++17)
+    Kompilacja: g++ -std=c++17 -O2 -Wall -Wextra main.cpp -o app
+*/
+
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+    std::printf("test");
+        std::cin.get();
+        return 0;
+}
